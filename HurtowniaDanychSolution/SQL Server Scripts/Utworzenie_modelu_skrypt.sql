0b346214db76e1aefe3be5a4e17bea38a3e127ae
ddavid09 <ddawidziak.dd@gmail.com>
2020-05-23 15:54:36 +0200

    dodanie procedury skladowanej do skryptu tworzacego model

--- HurtowniaDanychSolution/SQL Server Scripts/Utworzenie_modelu_skrypt.sql
+++ HurtowniaDanychSolution/SQL Server Scripts/Utworzenie_modelu_skrypt.sql
@@ -47,8 +47,8 @@ CREATE TABLE [dbo].[GEOGRAFIA_DIM]
 	[GEOGRAFIA_ID] [int] IDENTITY(1,1) NOT NULL,
 	[KONTYNENT] [nvarchar](50) NULL,
 	[KRAJ] [nvarchar](50) NULL,
-	[POPULACJA] [int] NULL,
-	[PKB] [int] NULL
+	[POPULACJA] [Bigint] NULL,
+	[PKB] [Bigint] NULL
 )
 GO
 
@@ -156,6 +156,54 @@ GO
 
 ALTER TABLE [dbo].[PACJENT_DIM] ADD
 	CONSTRAINT [FK_PACJENT_GEOGRAFIA] FOREIGN KEY ([GEOGRAFIA_ID]) REFERENCES [dbo].[GEOGRAFIA_DIM] ([GEOGRAFIA_ID]);
+GO
+
+-- *************************************************************************************************
+-- Utworzenie procedury skladowanej do dynamicznego tworzenia tabel stage dla danych z csse z github
+-- *************************************************************************************************
+
+GO
+DROP PROCEDURE IF EXISTS dbo.utworz_tabele_stage
+GO
+
+CREATE PROC dbo.utworz_tabele_stage
+
+@nazwa_tabeli nvarchar(200),
+@liczba_kolumn_daty int
+
+AS
+
+DECLARE @createtablesql nvarchar(max);
+
+SET @createtablesql = N'
+
+IF EXISTS (SELECT [name]
+	FROM [CovidHurtowniaDanych].[sys].[tables]
+	WHERE [name] = '''+ @nazwa_tabeli + ''')
+		DROP TABLE [' + @nazwa_tabeli + ']
+
+CREATE TABLE [dbo].[' + @nazwa_tabeli + ']
+(
+	[Province/State] [nvarchar](50) NULL,
+	[Country/Region] [nvarchar](50) NULL,
+	[Lat] [decimal](4) NULL,
+	[Long] [decimal](4) NULL,
+)';
+
+EXEC sys.sp_executesql @createtablesql;
+
+DECLARE @startdate DATE = '2020-01-22';
+DECLARE @addcolumnsql nvarchar(max);
+DECLARE @i int = 0;
+
+WHILE @i < @liczba_kolumn_daty
+BEGIN
+	set @addcolumnsql = N'ALTER TABLE [dbo].['+ @nazwa_tabeli +']
+						ADD [' + CAST(@startdate AS nvarchar(25)) + '] [int]';
+	EXEC sys.sp_executesql @addcolumnsql;
+SET @startdate = DATEADD(day, 1, @startdate);
+SET @i = @i + 1;
+END
 GO
 
 
