026a70cf96e07e5fbf6cb1b73022cb58b74f47a3
ddavid09 <ddawidziak.dd@gmail.com>
2020-05-24 23:55:04 +0200

    miara nowo-zainfekowani w tabeli faktow (tempo) proc.

--- HurtowniaDanychSolution/SQL Server Scripts/Zasilenie_tabeli_faktow_tempo.sql
+++ HurtowniaDanychSolution/SQL Server Scripts/Zasilenie_tabeli_faktow_tempo.sql
@@ -20,6 +20,8 @@ ALTER TABLE #process_deaths_table DROP COLUMN [Lat]
 ALTER TABLE #process_deaths_table DROP COLUMN [Long]
 
 --zagregowane przypadki CONFIRMED|RECOVERED|DEATHS wg krajow dla zadanej daty
+DROP TABLE IF EXISTS #process_fact
+
 SELECT
 C.[Country/Region],
 CONFIRMED,
@@ -48,12 +50,67 @@ GROUP BY [Country/Region]) AS D
 ON C.[Country/Region] = D.[Country/Region]
 ORDER BY CONFIRMED DESC
 
+--stage dla tabeli faktow
+
+DROP TABLE IF EXISTS [dbo].[stage_tempo_fact]
+
+CREATE TABLE [dbo].[stage_tempo_fact]
+(
+	[FAKT_ID] [int] IDENTITY(1,1) NOT NULL,
+	[CZAS_ID] [int] NULL,
+	[GEOGRAFIA_ID] [int] NULL,
+	[LICZBA_ZAKAZENI_OGOLEM] [int] NULL,
+	[LICZBA_ZGONOW_OGOLEM] [int] NULL,
+	[LICZBA_WYLECZONYCH_OGOLEM] [int] NULL,
+	[LICZBA_NOWYCH_ZAKAZEN_DZIS] [int] NULL,
+	[LICZBA_ZAKAZONYCH_NA_DZIS] [int] NULL,
+	[DYNAMIKA_ZAKAZEN] [float] NULL,
+	[NUMER_KOLEJNY_DNIA] [int] NULL
+)
+
+--czesciowe zaladowanie stage w celu latwiejszego policzenia nowych przypadkow i dynamiki
 --miary
 --liczba zakazonych na dany dzien
 ALTER TABLE #process_fact ADD INFECTED_ON_THAT_DAY AS (CONFIRMED - (DEATHS + RECOVERED))
-
-
+ALTER TABLE #process_fact ADD [DATE] AS '2020-05-23'
 SELECT * FROM #process_fact
+--**************************************************************************************
+
+INSERT INTO stage_tempo_fact
+(CZAS_ID, GEOGRAFIA_ID, LICZBA_ZAKAZENI_OGOLEM, LICZBA_ZGONOW_OGOLEM, LICZBA_WYLECZONYCH_OGOLEM, LICZBA_ZAKAZONYCH_NA_DZIS)
+SELECT 
+ISNULL(C.CZAS_ID, 0),
+ISNULL(G.GEOGRAFIA_ID, 0),
+ISNULL(P.CONFIRMED, 0),
+ISNULL(P.DEATHS, 0),
+ISNULL(P.RECOVERED, 0),
+ISNULL(P.INFECTED_ON_THAT_DAY, 0)
+FROM #process_fact AS P
+INNER JOIN CZAS_DIM AS C ON P.[DATE] = C.[DATA]
+INNER JOIN GEOGRAFIA_DIM AS G ON P.[Country/Region] = G.KRAJ
+
+SELECT * FROM stage_tempo_fact
+
+
+
+
+--liczba nowych przypadkow
+
+DROP TABLE IF EXISTS #process_fact1
+
+SELECT P.*, DB.LICZBA_ZAKAZENI_OGOLEM AS CONFIRMED_DAY_BEFORE
+INTO #process_fact1
+FROM #process_fact AS P
+INNER JOIN
+(SELECT K.KRAJ ,F.LICZBA_ZAKAZENI_OGOLEM FROM stage_tempo_fact AS F
+INNER JOIN GEOGRAFIA_DIM AS K ON F.GEOGRAFIA_ID = K.GEOGRAFIA_ID
+INNER JOIN CZAS_DIM AS C ON F.CZAS_ID = C.CZAS_ID
+WHERE C.[DATA] = '2020-05-22') AS DB
+ON P.[Country/Region] = DB.KRAJ
+
+ALTER TABLE #process_fact1 ADD NEW_INFECTED AS (CONFIRMED-CONFIRMED_DAY_BEFORE)
+
+SELECT * FROM #process_fact1
 
 --Utworzenie tymczasowej tabeli faktow
 DROP TABLE IF EXISTS #fact_tempo
